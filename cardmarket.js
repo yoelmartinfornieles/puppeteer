d06ff2b8daf73d2543ed11de6e5ed61d21645f2f
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-11-12 20:02:35 +0100

    feat: comments added

--- cardmarket.js
+++ cardmarket.js
@@ -1,20 +1,27 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+	//initialize the browser
 	const browser = await puppeteer.launch({headless: false});
+	//open a page
 	const page = await browser.newPage();
 
+	//go to a url
 	await page.goto('http://www.cardmarket.com/');
 
+	//click a button
 	const button = await page.$('.btn.btn-secondary.btn-sm');
 	await button.click();           
+	//take a screenshot
 	await page.screenshot({path: './testPics/cardmarketMain.jpg'})
 
+	//click a part in the webpage
 	const mtg = await page.$('.sprite-link.py-md-4');
 	await mtg.click(); 
 	await page.waitForTimeout(2000);
 	await page.screenshot({path: './testPics/mtg.jpg'})
 
+	//type a text in a form
 	await page.type('#ProductSearchInput', 'lightning bolt');	
 	await page.screenshot({path: './testPics/lightningboltsearch.jpg'})
 
@@ -23,10 +30,15 @@ const puppeteer = require('puppeteer');
 	await page.waitForTimeout(2000);
 	await page.screenshot({path: './testPics/lightningboltsearchresults.jpg'}) 
 
+	//click on a link
 	const card = await page.$('#productRow556319 > div:nth-child(4) ');
 	await card.click();
 	await page.waitForTimeout(2000);
 	await page.screenshot({path: './testPics/lightningboltcard.jpg'})
 
+	//wait for a part of the page to load
+	await page.waitForSelector('#image');
+
+	await page.waitForSelector('[data-toggle=tooltip]')
 /* 	await browser.close();
  */})();
